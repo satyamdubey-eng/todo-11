d59c11ef4ccde55c5412173d1037e986ef310590
satyam <satyam.dubey@fleekitsolutions.com>
2025-10-15 16:10:50 +0530

    this is demo

---
+++ src/main/java/Package1/Test1.java
@@ -0,0 +1,8 @@
+package Package1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("This is second");
+	}
+
+}
---
+++ src/test/java/Package2/Test2.java
@@ -0,0 +1,8 @@
+package Package2;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("this is first");
+	}
+
+}
